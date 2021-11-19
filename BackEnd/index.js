daa345d24f10f2e8d8b10c51ea842930bbccf2a3
MeLThRoX <meltgrox@pm.me>
2021-11-19 19:30:00 +0100

    Created flights Sanatized

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -6,8 +6,9 @@ global.config = require('./config')
 
 app.use(express.json());
 
-app.use('/api', require('./api/auth'))
+app.use('/api', require('./api/auth').router)
 app.use("/api/user", require('./api/routes/user'))
+app.use("/api/flights", require('./api/routes/flights'))
 
 app.listen(config.port, () => {
     console.log(`REST-API listening on port ${config.port}`)
